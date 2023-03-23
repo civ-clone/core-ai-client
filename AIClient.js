d61eb8368de99f9cb6de7157c8bb403f6ff751f0
dom111 <dom111@users.noreply.github.com>
2023-03-23 20:54:54 +0000

    Simpliy AIClient base class.

--- AIClient.js
+++ AIClient.js
@@ -1,40 +1,9 @@
 "use strict";
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
-    if (kind === "m") throw new TypeError("Private method is not writable");
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
-    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
-    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
-};
-var _AIClient_leaderRegistry, _AIClient_randomNumberGenerator;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AIClient = void 0;
 const Client_1 = require("@civ-clone/core-client/Client");
-const LeaderRegistry_1 = require("@civ-clone/core-civilization/LeaderRegistry");
 class AIClient extends Client_1.Client {
-    constructor(player, leaderRegistry = LeaderRegistry_1.instance, randomNumberGenerator = () => Math.random()) {
-        super(player);
-        _AIClient_leaderRegistry.set(this, void 0);
-        _AIClient_randomNumberGenerator.set(this, void 0);
-        __classPrivateFieldSet(this, _AIClient_leaderRegistry, leaderRegistry, "f");
-        __classPrivateFieldSet(this, _AIClient_randomNumberGenerator, randomNumberGenerator, "f");
-    }
-    chooseCivilization(choices) {
-        const Random = choices[Math.floor(choices.length * __classPrivateFieldGet(this, _AIClient_randomNumberGenerator, "f").call(this))], player = this.player(), civilization = new Random();
-        player.setCivilization(civilization);
-        this.chooseLeader(civilization);
-    }
-    chooseLeader(civilization) {
-        const leaders = __classPrivateFieldGet(this, _AIClient_leaderRegistry, "f").getByCivilization(civilization.constructor);
-        const RandomLeader = leaders[Math.floor(__classPrivateFieldGet(this, _AIClient_randomNumberGenerator, "f").call(this) * leaders.length)];
-        civilization.setLeader(new RandomLeader());
-    }
 }
 exports.AIClient = AIClient;
-_AIClient_leaderRegistry = new WeakMap(), _AIClient_randomNumberGenerator = new WeakMap();
 exports.default = AIClient;
 //# sourceMappingURL=AIClient.js.map
--- AIClient.ts
+++ AIClient.ts
@@ -1,53 +1,7 @@
 import { Client, IClient } from '@civ-clone/core-client/Client';
-import {
-  LeaderRegistry,
-  instance as leaderRegistryInstance,
-} from '@civ-clone/core-civilization/LeaderRegistry';
-import Civilization from '@civ-clone/core-civilization/Civilization';
-import { IConstructor } from '@civ-clone/core-registry/Registry';
-import Player from '@civ-clone/core-player/Player';
 
-export interface IAIClient extends IClient {
-  chooseCivilization(choices: typeof Civilization[]): void;
-  chooseLeader(civilization: Civilization): void;
-}
+export interface IAIClient extends IClient {}
 
-export class AIClient extends Client implements IAIClient {
-  #leaderRegistry: LeaderRegistry;
-  #randomNumberGenerator: () => number;
-
-  constructor(
-    player: Player,
-    leaderRegistry: LeaderRegistry = leaderRegistryInstance,
-    randomNumberGenerator: () => number = () => Math.random()
-  ) {
-    super(player);
-
-    this.#leaderRegistry = leaderRegistry;
-    this.#randomNumberGenerator = randomNumberGenerator;
-  }
-
-  chooseCivilization(choices: typeof Civilization[]): void {
-    const Random =
-        choices[Math.floor(choices.length * this.#randomNumberGenerator())],
-      player = this.player(),
-      civilization = new Random();
-
-    player.setCivilization(civilization);
-
-    this.chooseLeader(civilization);
-  }
-
-  chooseLeader(civilization: Civilization): void {
-    const leaders = this.#leaderRegistry.getByCivilization(
-      civilization.constructor as IConstructor<Civilization>
-    );
-
-    const RandomLeader =
-      leaders[Math.floor(this.#randomNumberGenerator() * leaders.length)];
-
-    civilization.setLeader(new RandomLeader());
-  }
-}
+export class AIClient extends Client implements IAIClient {}
 
 export default AIClient;
